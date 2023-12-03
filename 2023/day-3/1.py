9d0cb619d779fc2069669a818bcc4522d1f7448e
Ashish Kulkarni <ashish2002kulkarni@gmail.com>
2023-12-03 11:50:15 +0530

    day 3

---
+++ 2023/day-3/1.py
@@ -0,0 +1,25 @@
+import re
+
+res = 0
+
+with open("input.txt") as f:
+    lines = f.readlines()
+    for row, line in enumerate(lines):
+        matches = re.finditer(r"\d+", line)
+        for m in matches:
+            start, end, n = m.start(0), m.end(0), m.group(0)
+            num_res = 0
+            neighbours = ""
+            if row > 0:
+                neighbours += lines[row-1][max(0, start - 1):min(end + 1, len(line))]
+            if row < len(lines) - 1:
+                neighbours += lines[row+1][max(0, start - 1):min(end + 1, len(line))]
+            if start > 0:
+                neighbours += line[start-1]
+            if end < len(line):
+                neighbours += line[end]
+            symbol = neighbours.replace("\n", "").replace(".", "")
+            if symbol:
+                res += int(n)
+
+print(res)
---
+++ 2023/day-3/2.py
@@ -0,0 +1,36 @@
+import re
+import collections
+import math
+
+gears = collections.defaultdict(list)
+
+with open("input.txt") as f:
+    lines = f.readlines()
+    for row, line in enumerate(lines):
+        matches = re.finditer(r"\d+", line)
+        for m in matches:
+            start, end, n = m.start(0), m.end(0), m.group(0)
+            num_res = 0
+            neighbours = ""
+            if row > 0:
+                above = lines[row-1][max(0, start - 1):min(end + 1, len(line))]
+                for i, c in enumerate(above):
+                    if c == "*":
+                        gears[(row - 1, max(0, start - 1) + i)].append(int(n))
+            if row < len(lines) - 1:
+                below = lines[row+1][max(0, start - 1):min(end + 1, len(line))]
+                for i, c in enumerate(below):
+                    if c == "*":
+                        gears[(row + 1, max(0, start - 1) + i)].append(int(n))
+            if start > 0 and line[start-1] == "*":
+                gears[(row, start - 1)].append(int(n))
+            if end < len(line) and line[end] == "*":
+                gears[(row, end)].append(int(n))
+
+res = 0
+
+for _, nums in gears.items():
+    if len(nums) == 2:
+        res += math.prod(nums)
+
+print(res)
