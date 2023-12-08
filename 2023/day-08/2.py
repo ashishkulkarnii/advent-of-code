c1a1edd78e491be67fec3c79c2cd8fd3ddcaac25
Ashish Kulkarni <ashish2002kulkarni@gmail.com>
2023-12-08 11:04:52 +0530

    day 8

--- 2023/day-08/2.py
+++ 2023/day-08/2.py
@@ -1,3 +1,5 @@
+import math
+
 with open("input.txt") as f:
     line1 = f.readline().replace("\n", "")
     f.readline()
@@ -16,14 +18,31 @@ for key in graph.keys():
     if key[-1] == "A":
         currs.append(key)
 
-ptr = 0
 l = len(line1)
 
-while not all([curr[-1] == "Z" for curr in currs]):
-    if line1[ptr % l] == "R":
-        currs = [graph[curr][1] for curr in currs]
-    else:
-        currs = [graph[curr][0] for curr in currs]
-    ptr += 1
+# # bruteforce
+# while not all([curr[-1] == "Z" for curr in currs]):
+#     print(currs)
+#     if line1[ptr % l] == "R":
+#         currs = [graph[curr][1] for curr in currs]
+#     else:
+#         currs = [graph[curr][0] for curr in currs]
+#     ptr += 1
 
-print(ptr)
+# LCM, may not work in all cases
+ptrs = []
+for curr in currs:
+    ptr = 0
+    while curr[-1] != "Z":
+        if line1[ptr % l] == "R":
+            curr = graph[curr][1]
+        else:
+            curr = graph[curr][0]
+        ptr += 1
+    ptrs.append(ptr)
+
+print(ptrs)
+
+res = math.lcm(*ptrs)
+
+print(res)
